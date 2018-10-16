66fd92b34a7254003aa214b8b135ee78c6558a8b
jakubiakm <maciej190509@gmail.com>
2018-10-16 21:43:47 +0200

    Add project files.

---
+++ GK3D.Lab1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GK3D.Lab1
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new CosmoGame())
+                game.Run();
+        }
+    }
+}
