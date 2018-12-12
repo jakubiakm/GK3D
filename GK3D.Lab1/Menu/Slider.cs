d6737cc058cf3d79b489a8286f32b1bb75d94003
jakubiakm <maciej190509@gmail.com>
2018-12-12 22:00:59 +0100

    rysowanie slidera

--- GK3D.Lab1/Menu/Slider.cs
+++ GK3D.Lab1/Menu/Slider.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GK3D.Lab1.Helpers;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace GK3D.Lab1.Menu
@@ -40,6 +42,16 @@ namespace GK3D.Lab1.Menu
 
         public void Draw(SpriteBatch spriteBatch, GraphicsDevice graphicsDevice, int x, int y, int width, int height)
         {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            spriteBatch.DrawString(_font, $"{Content}", new Vector2(x, y), Color.Red);
+            DrawRectangleHelper.DrawRoundedRectangle(spriteBatch, graphicsDevice, x, y + 20 + height / 2, width, height / 10, new Color(0, 0, 0, 244));
+            float percent = (float)(Value - Min) / (Max - Min);
+            int sliderDragXPosition = x + 10 + (int)(percent * (Width));
+            DrawRectangleHelper.DrawRoundedRectangle(spriteBatch, graphicsDevice, sliderDragXPosition, y + 20, 10, height, new Color(0, 0, 0, 244));
+
         }
 
         public void Update()
